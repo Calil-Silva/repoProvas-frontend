7d8865553ea932e2c6f07154f23f8c63e41be96d
Calil-Silva <ka.renner@hotmail.com>
2021-12-19 18:11:45 -0300

    Created creation storage

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import TestCreation from "./pages/TestCreation";
 
 function App() {
   const [tests, setTests] = useState([]);
+  const [testsParams, setTestsParams] = useState({});
 
   return (
     <BrowserRouter>
       <GlobalStyles />
-      <TestContext.Provider value={{ tests, setTests }}>
+      <TestContext.Provider
+        value={{ tests, setTests, testsParams, setTestsParams }}
+      >
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/test-creation" element={<TestCreation />} />
--- src/pages/TestCreation.js
+++ src/pages/TestCreation.js
@@ -1,8 +1,16 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import TestContext from "../contexts/TestContext";
+import { postTestsParams } from "../services/repoProvas";
+import { getTestCreationParams } from "../services/testCreationPersistence";
 
 export default function TestCreation() {
-  const { tests } = useContext(TestContext);
+  const { testsParams, setTestsParams } = useContext(TestContext);
+  const { categoriesNames, periodsNames, professorsBySubject } = testsParams;
+
+  useEffect(() => {
+    setTestsParams(getTestCreationParams());
+  }, [setTestsParams]);
+
   const [newTest, setNewTest] = useState({
     name: "",
     category: "",
@@ -12,7 +20,23 @@ export default function TestCreation() {
     period: "",
   });
 
-  console.log(newTest);
+  const subjects = professorsBySubject?.reduce((arr, cur) => {
+    arr.push(cur.subject);
+
+    return [...new Set(arr)];
+  }, []);
+
+  const professors = professorsBySubject
+    ?.filter(({ subject }) => subject === newTest.subject)
+    .map(({ professor }) => professor);
+
+  function testSubmit(e) {
+    e.preventDefault();
+
+    postTestsParams(newTest)
+      .then(() => alert("Prova criada com sucesso"))
+      .catch(() => alert("Ocorreu um erro, tente novamente"));
+  }
 
   return (
     <form>
@@ -21,31 +45,70 @@ export default function TestCreation() {
         placeholder="Nome da prova"
         onChange={(e) => setNewTest({ ...newTest, name: e.target.value })}
       />
-      <input
-        type="text"
-        placeholder="Período"
-        onChange={(e) => setNewTest({ ...newTest, period: e.target.value })}
-      />
-      <input
-        type="text"
-        placeholder="Categoria"
-        onChange={(e) => setNewTest({ ...newTest, category: e.target.value })}
-      />
-      <input
-        type="text"
-        placeholder="Disciplina"
-        onChange={(e) => setNewTest({ ...newTest, subject: e.target.value })}
-      />
-      <input
-        type="text"
-        placeholder="Professor"
-        onChange={(e) => setNewTest({ ...newTest, professor: e.target.value })}
-      />
+      <select
+        onChange={(e) =>
+          setNewTest({
+            ...newTest,
+            period: periodsNames[e.target.selectedIndex - 1],
+          })
+        }
+      >
+        <option>-selecione-</option>
+        {periodsNames?.map((period) => (
+          <option key={period}>{period}</option>
+        ))}
+      </select>
+
+      <select
+        onChange={(e) =>
+          setNewTest({
+            ...newTest,
+            category: categoriesNames[e.target.selectedIndex - 1],
+          })
+        }
+      >
+        <option>-selecione-</option>
+        {categoriesNames?.map((category) => (
+          <option key={category}>{category}</option>
+        ))}
+      </select>
+
+      <select
+        onChange={(e) =>
+          setNewTest({
+            ...newTest,
+            subject: subjects[e.target.selectedIndex - 1],
+            professor: "",
+          })
+        }
+      >
+        <option>-selecione-</option>
+        {subjects?.map((subject) => (
+          <option key={subject}>{subject}</option>
+        ))}
+      </select>
+
+      <select
+        onChange={(e) =>
+          setNewTest({
+            ...newTest,
+            professor: professors[e.target.selectedIndex - 1],
+          })
+        }
+      >
+        <option>-selecione-</option>
+        {professors?.map((professor) => (
+          <option key={professor}>{professor}</option>
+        ))}
+      </select>
+
       <input
         type="text"
         placeholder="Link"
         onChange={(e) => setNewTest({ ...newTest, link: e.target.value })}
       />
+
+      <input type="submit" value="Enviar" onClick={testSubmit} />
     </form>
   );
 }
